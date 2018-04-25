99cc6d43d1e4facc09fa9f026fc11f4169e2fd12
Katy Bowers <katyvine@gmail.com>
2018-04-24 17:17:08 -0700

    add gitignore and update scripts

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,3 +1,15 @@
-ENV {
-  cloudAPI: 'https://kb-ka-booklist.herokuapp.com'
-}
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://kb-ka-booklist.herokuapp.com';
+ENV.developmentApiUrl = 'http://localhost:3000';
+
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+// if(ENV.isProduction){
+//   ENV.apiUrl = ENV.productionApiUrl;
+// } else{
+//   ENV.apiUrl = ENV.developmentAprUrl;
+// }
+
+
