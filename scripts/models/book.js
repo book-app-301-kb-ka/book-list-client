071a0f01257c478086c170b1aa11ff0e0f9cccf6
Katy Bowers <katyvine@gmail.com>
2018-04-26 11:11:00 -0700

    working on one book view

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -26,12 +26,23 @@ Book.prototype.toHtml = function(){
   return template(this);
 };
 
+Book.prototype.detailToHtml = function(){
+  var template = Handlebars.compile($('#book-detail-template').text());
+  return template(this);
+};
+
 Book.loadAll = rows => Book.all = rows.sort((a,b) => b.title - a.title).map(book => new Book(book));
  
 Book.fetchAll = callback => {
-  // console.log(ENV.apiUrl);
   $.get(`${ENV.apiUrl}/api/v1/books`)
   .then(Book.loadAll)
+  .then(callback)
+  .catch(errorCallback);
+}
+
+Book.fetchOne = (callback, ctx) => {
+  $.get(`${ENV.apiUrl}/api/v1/books/${ctx.params.book_id}`)
+  .then(results => ctx.book = results[0])
   .then(callback)
   .catch(errorCallback);
 }
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -10,11 +10,23 @@ var app = app || {};
     app.Book.all.map(a => $('#book-list').append(a.toHtml()));
 }
 
+  bookView.initDetailPage = function(ctx) {
+    $('.container').hide();
+    $('.book-view').hide();
+    $('.detail-view').show();
+    app.Book.all.forEach(a => {
+      if (parseInt(a.id) === parseInt(ctx.params.book_id)){
+        $('.detail-view').append(a.detailToHtml());
+      }   
+    })
+  }
+
   module.bookView = bookView;
 }) (app)
 
 $(function() {
   app.Book.fetchAll(app.bookView.initIndexPage);
+  app.Book.fetchOne(app.bookView.initDetailPage);
 })
 
 // $(document).ready(function() {
